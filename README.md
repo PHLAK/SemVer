c8bd52f7239c9b47b63ff529de8f06d6849c4feb
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-06-23 18:37:23 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,62 @@
+SemVer
+======
+
+[![Latest Stable Version](https://img.shields.io/packagist/v/PHLAK/Stash.svg)](https://packagist.org/packages/PHLAK/Stash)
+[![Total Downloads](https://img.shields.io/packagist/dt/PHLAK/Stash.svg)](https://packagist.org/packages/PHLAK/Stash)
+[![Author](https://img.shields.io/badge/author-Chris%20Kankiewicz-blue.svg)](https://www.ChrisKankiewicz.com)
+[![License](https://img.shields.io/packagist/l/PHLAK/Stash.svg)](https://packagist.org/packages/PHLAK/Stash)
+[![Build Status](https://img.shields.io/travis/PHLAK/Stash.svg)](https://travis-ci.org/PHLAK/Stash)
+
+[Semantic versioning](http://semver.org) helper library for PHP -- by, [Chris Kankiewicz](https://www.ChrisKankiewicz.com)
+
+Introduction
+------------
+
+More info coming soon...
+
+Requirements
+------------
+
+  - [PHP](https://php.net) >= 5.4
+
+
+Install with Composer
+---------------------
+
+```bash
+composer require phlak/semver
+```
+
+Usage
+-----
+
+More info coming soon...
+
+Troubleshooting
+---------------
+
+Please report bugs to the [GitHub Issue Tracker](https://github.com/PHLAK/SemVer/issues).
+
+-----
+
+MIT License
+
+**Copyright (c) 2016 Chris Kankewicz <Chris@ChrisKankiewicz.com>**
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ tests/SemVerTest.php
@@ -0,0 +1,74 @@
+<?php
+
+class SemVerTest extends PHPUnit_Framework_TestCase {
+
+    /** @test */
+    public function it_can_be_initialized() {
+        $this->assertInstanceOf('SemVer\SemVer', new SemVer\SemVer);
+    }
+
+    /** @test */
+    public function it_can_set_and_retrieve_a_version() {
+        $semver = (new SemVer\SemVer)->setVersion('v1.3.37');
+        $this->assertEquals('v1.3.37', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_increment_major() {
+        $semver = (new SemVer\SemVer('v1.3.37'))->incrementMajor();
+        $this->assertEquals('v2.0.0', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_set_major() {
+        $semver = (new SemVer\SemVer('v1.3.37'))->setMajor(7);
+        $this->assertEquals('v7.0.0', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_increment_minor() {
+        $semver = (new SemVer\SemVer('v1.3.37'))->incrementMinor();
+        $this->assertEquals('v1.4.0', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_set_minor() {
+        $semver = (new SemVer\SemVer('v1.3.37'))->setMinor(5);
+        $this->assertEquals('v1.5.0', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_increment_patch() {
+        $semver = (new SemVer\SemVer('v1.3.37'))->incrementPatch();
+        $this->assertEquals('v1.3.38', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_set_patch() {
+        $semver = (new SemVer\SemVer('v1.3.37'))->setPatch(12);
+        $this->assertEquals('v1.3.12', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_set_pre() {
+        $semver = (new SemVer\SemVer)->setPreRelease('alpha.5');
+        $this->assertEquals('v0.1.0-alpha.5', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_set_build() {
+        $semver = (new SemVer\SemVer)->setBuild('007');
+        $this->assertEquals('v0.1.0+007', $semver->getVersion());
+    }
+
+    /** @test */
+    public function it_can_get_properties_independently() {
+        $semver = new SemVer\SemVer('v1.3.37-alpha.5+007');
+        $this->assertEquals(1, $semver->getMajor());
+        $this->assertEquals(3, $semver->getMinor());
+        $this->assertEquals(37, $semver->getPatch());
+        $this->assertEquals('alpha.5', $semver->getPreRelease());
+        $this->assertEquals('007', $semver->getBuild());
+    }
+
+}
