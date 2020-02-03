d67885a988ed2ae33693db5a3a98c2bede2c21af
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2020-02-02 18:54:46 -0700

    Cleaned up / tweaked comparePrerelease function

--- src/Traits/Comparable.php
+++ src/Traits/Comparable.php
@@ -14,7 +14,7 @@ trait Comparable
      * @return bool True if this Version object is greater than the comparing
      *              object, otherwise false
      */
-    public function gt(Version $version): bool
+    public function gt(Version $version) : bool
     {
         // compare without prerelease
         $thisVersion = [$this->major, $this->minor, $this->patch];
@@ -22,7 +22,7 @@ trait Comparable
         $result = $thisVersion <=> $thatVersion;
         // if both version are equal we should compare pre release only if both property are sets
         if (0 === $result) {
-            $result = $this->comparePrerelease($this->preRelease, $version->preRelease);
+            $result = $this->comparePreReleases($this->preRelease, $version->preRelease);
         }
 
         return 1 === $result;
@@ -36,14 +36,14 @@ trait Comparable
      * @return bool True if this Version object is less than the comparing
      *              object, otherwise false
      */
-    public function lt(Version $version): bool
+    public function lt(Version $version) : bool
     {
         $thisVersion = [$this->major, $this->minor, $this->patch];
         $thatVersion = [$version->major, $version->minor, $version->patch];
         $result = $thisVersion <=> $thatVersion;
         // if both version are equal we should compare pre release only if both property are sets
         if (0 === $result) {
-            $result = $this->comparePrerelease($this->preRelease, $version->preRelease);
+            $result = $this->comparePreReleases($this->preRelease, $version->preRelease);
         }
 
         return $result === -1;
@@ -57,7 +57,7 @@ trait Comparable
      * @return bool True if this Version object is equal to the comparing
      *              object, otherwise false
      */
-    public function eq(Version $version): bool
+    public function eq(Version $version) : bool
     {
         $thisVersion = [$this->major, $this->minor, $this->patch, $this->preRelease];
         $thatVersion = [$version->major, $version->minor, $version->patch, $version->preRelease];
@@ -73,7 +73,7 @@ trait Comparable
      * @return bool True if this Version object is not equal to the comparing
      *              object, otherwise false
      */
-    public function neq(Version $version): bool
+    public function neq(Version $version) : bool
     {
         $thisVersion = [$this->major, $this->minor, $this->patch, $this->preRelease];
         $thatVersion = [$version->major, $version->minor, $version->patch, $version->preRelease];
@@ -89,14 +89,14 @@ trait Comparable
      * @return bool True if this Version object is greater than or equal to the
      *              comparing object, otherwise false
      */
-    public function gte(Version $version): bool
+    public function gte(Version $version) : bool
     {
         $thisVersion = [$this->major, $this->minor, $this->patch];
         $thatVersion = [$version->major, $version->minor, $version->patch];
         $result = $thisVersion <=> $thatVersion;
         // if both version are equal we should compare pre release only if both property are sets
         if (0 === $result) {
-            $result = $this->comparePrerelease($this->preRelease, $version->preRelease);
+            $result = $this->comparePreReleases($this->preRelease, $version->preRelease);
         }
 
         return $result >= 0;
@@ -110,52 +110,43 @@ trait Comparable
      * @return bool True if this Version object is less than or equal to the
      *              comparing object, otherwise false
      */
-    public function lte(Version $version): bool
+    public function lte(Version $version) : bool
     {
         $thisVersion = [$this->major, $this->minor, $this->patch];
         $thatVersion = [$version->major, $version->minor, $version->patch];
         $result = $thisVersion <=> $thatVersion;
         // if both version are equal we should compare pre release only if both property are sets
         if (0 === $result) {
-            $result = $this->comparePrerelease($this->preRelease, $version->preRelease);
+            $result = $this->comparePreReleases($this->preRelease, $version->preRelease);
         }
 
         return $result <= 0;
     }
 
     /**
-     * return 1 if $prerelease is greater than $prerelease, 0 if equal else -1.
+     * Compare two pre-releases.
      *
-     * @param $prerelease1
-     * @param $prerelease2
+     * @param string|null $preRelease1
+     * @param string|null $preRelease2
      *
      * @return int
      */
-    private function comparePrerelease($prerelease1, $prerelease2): int
+    private function comparePreReleases($preRelease1, $preRelease2) : int
     {
-        // empty pre-release is greater than filled one
-        if ($prerelease1 === null && $prerelease2 !== null) {
-            return 1;
-        }
-        if ($prerelease1 !== null && $prerelease2 === null) {
+        if ($preRelease1 !== null && $preRelease2 === null) {
             return -1;
         }
-        // pre-release can be composed of multiple pre-release identifier like alpha.beta.1 or .beta.11
-        // beta.2 is smaller than beta.11 we should split each identifier and compare them
-        $prereleaseList1 = explode('.', $prerelease1);
-        $prereleaseList2 = explode('.', $prerelease2);
-        // both list must have the same size
-        $prereleaseList1 = array_pad($prereleaseList1, count($prereleaseList2), null);
-        $prereleaseList2 = array_pad($prereleaseList2, count($prereleaseList1), null);
-        $result = 0;
-        foreach ($prereleaseList1 as $idx => $pre1) {
-            $pre2 = $prereleaseList2[$idx];
-            $result = $pre1 <=> $pre2;
-            if (0 !== $result) {
-                return $result;
-            }
+
+        if ($preRelease1 === null && $preRelease2 !== null) {
+            return 1;
         }
 
-        return $result;
+        $preReleases1 = explode('.', $preRelease1 ?? '');
+        $preReleases2 = explode('.', $preRelease2 ?? '');
+
+        $preReleases1 = array_pad($preReleases1, count($preReleases2), null);
+        $preReleases2 = array_pad($preReleases2, count($preReleases1), null);
+
+        return $preReleases1 <=> $preReleases2;
     }
 }
