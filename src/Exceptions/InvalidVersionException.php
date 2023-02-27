20070d8bc5790ceb1e25a057c8828462e7285e9a
Dr. Harbich <ronny@developer.falconiform.de>
2023-02-27 23:42:32 +0100

    Update src/Exceptions/InvalidVersionException.php
    Co-authored-by: Chris Kankiewicz <Chris@ChrisKankiewicz.com>

--- src/Exceptions/InvalidVersionException.php
+++ src/Exceptions/InvalidVersionException.php
@@ -4,9 +4,6 @@ namespace PHLAK\SemVer\Exceptions;
 
 use Exception;
 
-/**
- * Exception that is thrown when an invalid semantic version string is used.
- */
 class InvalidVersionException extends Exception
 {
 }
